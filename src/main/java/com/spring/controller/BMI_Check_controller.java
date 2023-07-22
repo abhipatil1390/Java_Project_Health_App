b8eccd97e03de51443493c525a1625d530998e14
abhishek patil <ap50001390@gmail.com>
2023-07-22 16:53:38 +0530

    java prject

--- src/main/java/com/spring/controller/BMI_Check_controller.java
+++ src/main/java/com/spring/controller/BMI_Check_controller.java
@@ -1,7 +1,5 @@
 package com.spring.controller;
 
-import java.text.DecimalFormat;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
